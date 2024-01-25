41c86eed7e49abaa4d16c7292d69529abaaf3761
Logic-Koike <koike@logic-biz.co.jp>
2024-01-25 13:53:39 +0900

    テスト用GitHub Action追加(失敗想定)

---
+++ test/fail_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_app/models/favorites.dart';
+import 'package:testing_app/screens/favorites.dart';
+
+void main() {
+  group("This is fail test", () {
+    test("Test_Fail", () {
+      expect(1, 2);
+    });
+  });
+}
